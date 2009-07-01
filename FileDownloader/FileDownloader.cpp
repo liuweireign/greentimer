819690639f1fa89f241f39ec8d99ef31dd6db88c
yurihua <yurihua@5e73b346-014d-0410-b5d2-e30323ecab83>
2009-07-01 14:40:19 +0000

    Refined the progress bar.

--- FileDownloader/FileDownloader.cpp
+++ FileDownloader/FileDownloader.cpp
@@ -12,25 +12,26 @@ int OnProgress(void *clientp, double dltotal, double
         dlnow, double ultotal, double ulnow)
 {
 	int iLen = dlnow*60/dltotal;
-	for(int i=0;i<60;i++)
+
+	for(int i=0;i<71;i++)
+	printf("%c", 0x8);
+
+	for(int i=0;i<=60;i++)
 	{
-		if(i<=iLen) cout<<"/";
+		if (i == 0 || i==60) cout << "|";
+	    else if(i<iLen) cout<<"*";
+		else if(i==iLen) cout << ">";
 		else cout<<"-";
 	}
-	cout<<endl;
+
+	printf("\t%2.2f%%", 100*dlnow/dltotal); 
+
     return 0;
 }
 
 //当数据到达，写入磁盘里，并且显示下载的块数（以点号表示）
 size_t OnData( void *ptr, size_t size, size_t nmemb, void *stream)
 {
-	//cout<<(char *)ptr<<endl;
-	/*for(int i=0;i<2*size*nmemb/512;i++)
-	{
-		cout<<".";
-	}
-	cout<<endl;*/
-
 	ofstream &ofs = *(ofstream *)stream;
 	ofs.write((char *)ptr,size*nmemb);
 	return size*nmemb;
@@ -70,6 +71,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout<<"开始下载："<<strUrl<<endl;
 	cout<<"保存位置："<<strSavePath<<endl;
 
+	for(int i=0;i<=60;i++)
+	{
+		if (i == 0 || i==60) cout << "|";
+		else if((i*10/6)%10 == 0) cout<<"+";
+		else cout<<"-";
+	}
+
+	cout << endl;
+
 	//阻塞执行下载操作（如果是在一个线程中调用，可以达到不阻塞的目的）
 	res = curl_easy_perform(curl);
 	if(0!=res)
@@ -81,6 +91,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	curl_easy_cleanup(curl);
 
 	ofs.flush();
+	cout<<endl;
 	cout<<"下载完成："<<strSavePath<<endl;
 	return 0;
 }
