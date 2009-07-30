b3743066a6eda4b62e61e475e193f914073460ad
yurihua <yurihua@5e73b346-014d-0410-b5d2-e30323ecab83>
2009-07-30 12:37:39 +0000

    The project is used to test the httpsync class without the help of the Greentimer main program.

---
+++ trunk/testhttpsync/stdafx.h
@@ -0,0 +1,38 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是常用但不常更改的项目特定的包含文件
+//
+
+#pragma once
+
+#define WINVER		0x0500
+#define _WIN32_WINNT	0x0501
+#define _WIN32_IE	0x0600
+#define _RICHEDIT_VER	0x0200
+
+#include <atlbase.h>
+#include <atlstr.h>
+#include <atlapp.h>
+
+#define _WTL_NO_CSTRING
+
+#include <atlwin.h>
+#include <atlmisc.h>
+#include <atlcrack.h>
+#include <atlctrls.h>
+#include <atlframe.h>
+#include <atltime.h>
+#define END_MSG_MAP_EX	END_MSG_MAP
+
+
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <list>
+#include <set>
+#include <vector>
+#include <tchar.h>
+
+using namespace std;
+
+// TODO: 在此处引用程序要求的附加头文件
