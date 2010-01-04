724ca1ee4067c4070a3068c365b5603a4e49b6c8
tantianzuo <tantianzuo@5e73b346-014d-0410-b5d2-e30323ecab83>
2010-01-04 08:05:50 +0000

    增加一个本地HTTP服务器。现在通过http://localhost:8080/{filepath}的方式，即可读取本地文件。 目的有二： 1)提供网页屏蔽功能，当用户访问某些浪费时间的网页的时候，自动导向绿色时间任务页，显示任务列表； 2)脱离WTL。将来所有的配置和输入，都可以通过网页编程实现，加快开发速度。 接下来的问题： 1)首先，安全性！确保不会有缓冲区溢出之类的漏洞，避免用户受攻击。 2)返回的HTTP头数据，如文件类型、编码类型等等，必须填写正确，否则浏览器显示不正确。

---
+++ BallonMessager/SimpHTTPServ/ServThread.cpp
@@ -0,0 +1,50 @@
+#include ".\servthread.h"
+#include "UploadHandler.h"
+#include <assert.h>
+
+
+class SP_HttpEchoHandlerFactory : public SP_HttpHandlerFactory {
+public:
+	SP_HttpEchoHandlerFactory(){
+	}
+	virtual ~SP_HttpEchoHandlerFactory(){}
+
+	virtual SP_HttpHandler * create() const {
+		return new UploadHandler();
+	}
+private:
+};
+
+ServThread::ServThread()
+{
+	m_pSpServer = NULL;
+}
+
+ServThread::~ServThread(void)
+{
+}
+
+void * ServThread::Thread()
+{
+	ThreadStarted();
+
+	int port = 8080, maxThreads = 10;
+	const char * serverType = "hahs";
+
+	if( 0 != sp_initsock() ) assert( 0 );
+
+	m_pSpServer = new SP_IocpServer( "", port, new SP_HttpHandlerAdapterFactory( new SP_HttpEchoHandlerFactory() ) );
+
+	m_pSpServer->setTimeout( 60 );
+	m_pSpServer->setMaxThreads( maxThreads );
+	m_pSpServer->setReqQueueSize( 100, "HTTP/1.1 500 Sorry, server is busy now!\r\n" );
+	m_pSpServer->runForever();
+
+	sp_closelog();
+	return NULL;
+}
+
+void ServThread::shutdown()
+{
+	m_pSpServer->shutdown();
+}
